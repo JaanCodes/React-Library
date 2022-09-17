938bc9ae3804e2c161e2f7b7b6d98160f6e00b4e
Jan <janvilaaregut@gmail.com>
2022-09-17 11:06:07 +0200

    React Library

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -5,53 +5,23 @@ import Price from "./Price";
 import Rating from "./Rating";
 
 const Book = ({ book }) => {
-  const [img, setImg] = useState();
-  const mountedRef = useRef(true);
-
-  useEffect(() => {
-    const image = new Image();
-    image.src = book.url;
-    image.onload = () => {
-      if (mountedRef.current) {
-        setImg(image);
-      }
-    };
-    return () => {
-      mountedRef.current = false;
-    };
-  });
-
   return (
     <div className="book">
-      {img ? (
-        <>
-          <Link to={`/books/${book.title.toLowerCase()}`}>
-            <figure className="book__img--wrapper">
-              <img src={img.src} alt="" className="book__img" />
-            </figure>
-          </Link>
-          <div className="book__title">
-            <Link
-              to={`/books/${book.title.toLowerCase()}`}
-              className="book__title--link"
-            >
-              {book.title}
-            </Link>
-          </div>
-          <Rating rating={book.rating} />
-          <Price
-            salePrice={book.salePrice}
-            originalPrice={book.originalPrice}
-          />
-        </>
-      ) : (
-        <>
-          <div className="book__img--skeleton"></div>
-          <div className="skeleton book__title--skeleton"></div>
-          <div className="skeleton book__rating--skeleton"></div>
-          <div className="skeleton book__price--skeleton"></div>
-        </>
-      )}
+      <Link to={`/books/${book.title.toLowerCase()}`}>
+        <figure className="book__img--wrapper">
+          <img src={book.url} alt="" className="book__img" />
+        </figure>
+      </Link>
+      <div className="book__title">
+        <Link
+          to={`/books/${book.title.toLowerCase()}`}
+          className="book__title--link"
+        >
+          {book.title}
+        </Link>
+      </div>
+      <Rating rating={book.rating} />
+      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
     </div>
   );
 };
